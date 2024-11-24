a0741598377dcf6c48e21e98cf7c3449986161eb
Anigilator322 <Anigilator322@gmail.com>
2024-11-24 23:39:02 +0700

    Add PassUsingModel

---
+++ WebLab11/Controllers/CalcDemonstrationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebLab11.Models;
+
+namespace WebLab11.Controllers
+{
+    public class CalcDemonstrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PassUsingModel()
+        {
+            Random random = new Random();
+            int a = random.Next(11);
+            int b = random.Next(11);
+            var viewModel = new CalcDemonstrationViewModel()
+            {
+                FirstNum = a,
+                SecondNum = b,
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebLab11/Models/CalcDemonstrationViewModel.cs
@@ -0,0 +1,54 @@
+﻿namespace WebLab11.Models
+{
+    public class CalcDemonstrationViewModel
+    {
+        public int FirstNum {  get; set; }
+        public int SecondNum {  get; set; }
+        public int Addition 
+        {
+            get
+            {
+                return FirstNum + SecondNum;
+            }
+            private set
+            {
+                Addition = value;
+            }
+        }
+        public float Division 
+        {  
+            get
+            {
+                if(SecondNum == 0)
+                    return float.PositiveInfinity;
+                return (float)FirstNum / (float)SecondNum;
+            }
+            private set
+            {
+                Division = value;
+            }
+        }
+        public int Multiplication
+        {  
+            get
+            {
+                return FirstNum * SecondNum;
+            }
+            private set 
+            {
+                Multiplication = value;
+            }
+        }
+        public int Subtraction 
+        {  
+            get
+            {
+                return FirstNum - SecondNum;
+            }
+            private set
+            {
+                Subtraction = value;
+            }
+        }
+    }
+}
